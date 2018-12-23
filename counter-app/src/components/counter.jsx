a167fbda70105aa55c86b2659b6f298d8bc8ac40
asithw <mail.asithw@gmail.com>
2018-12-23 17:45:34 +0545

    inline styles

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,13 +1,31 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
 
-class Counter extends Component{
-    state ={};
-    render(){
-        return ('<h2>Hellow world  </h2>');
-    }
+class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  customStyle = {
+    marginTop: 20,
+    fontSize: "19px",
+    fontWeight: "bold"
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        {/* expression is something thta produces a value*/}
+        <span style={this.customStyle}>{this.state.count} world 1 </span>
+        <button style={ {fontSize:50} } className="btn btn-primary">Hello {this.formatCount()} </button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <u>test</u> : count;
+    //return count===0?"zero":count;
+  }
 }
 
-
 export default Counter;
- 
-

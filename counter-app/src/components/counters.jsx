25dbc0dfdf1cc40b379090f6a4e20becacf48bb9
asithw <mail.asithw@gmail.com>
2018-12-25 00:30:13 +0545

    Single source of truth.  reset does not update sub coponents

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -2,19 +2,33 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
+    
   state = {
     counters: [{ id: 0, value: 0 }, { id: 1, value: 2 }, { id: 2, value: 3 }]
   };
 
-  handleDelete = (id) => {
+  handleDelete = id => {
     console.log(id);
-    const counters = this.state.counters.filter(x => x.id !==id);
-    this.setState({counters});
-  }
+    const counters = this.state.counters.filter(x => x.id !== id);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    console.log("reset");
+    const counters = this.state.counters.map(y => {
+      y.value = 0;
+      return y;
+    });
+
+    this.setState({ counters });
+  };
 
   render() {
     return (
       <div>
+        <button className="btn btn-primary" onClick={this.handleReset}>
+          Reset
+        </button>
         {this.state.counters.map(x => (
           <Counter
             key={x.id}

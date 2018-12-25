bd612d2f0dfc32ba2cf67f8ea28598a4b1c9145f
asithw <mail.asithw@gmail.com>
2018-12-25 13:21:12 +0545

    state lifiting using single state event bubling to parent components

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,26 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-//import '../node_modules/bo'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Counters from "./components/counters";
+import NavBar from "./components/navBar";
+
 class App extends Component {
+  state = {
+    counters: [{ id: 0, value: 0 }, { id: 1, value: 2 }, { id: 2, value: 3 }]
+  };
+
+  handleIncrement = counter => {
+    //https://stackoverflow.com/questions/23436437/why-can-i-change-value-of-a-constant-in-javascript
+
+    const counters = [...this.state.counters]; //cloning
+    const index = counters.indexOf(counter);
+    // counters[0].value++; //THIS WILL CHNAGE STATE property also....
+
+    counters[index] = { ...counter }; // only clone the object
+    counters[index].value++;
+
+    console.log(
+      "after increment.. state value = ",
+      this.state.counters[0].value
+    );
+    console.log("after increment.. const value = ", counters[0].value);
+    this.setState({ counters });
+  };
+
+  handleDelete = counter => {
+    console.log(counter.id);
+    const counters = this.state.counters.filter(x => x.id !== counter.id);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    console.log("reset");
+    const counters = this.state.counters.map(y => {
+      y.value = 0;
+      return y;
+    });
+
+    this.setState({ counters });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <NavBar totalCounters = {this.state.counters.filter(x => x.value !==0).length}/>
+        <main className="containers">
+          <Counters
+            counters = {this.state.counters}
+            onIncrement={this.handleIncrement}
+            onReset={this.handleReset}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
     );
   }
 }
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -2,54 +2,56 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [{ id: 0, value: 0 }, { id: 1, value: 2 }, { id: 2, value: 3 }]
-  };
 
-  handleIncrement = counter => {
-    //https://stackoverflow.com/questions/23436437/why-can-i-change-value-of-a-constant-in-javascript
 
-    const counters = [...this.state.counters]; //cloning
-const index = counters.indexOf(counter);
-    // counters[0].value++; //THIS WILL CHNAGE STATE property also....
+  // handleIncrement = counter => {
+  //   //https://stackoverflow.com/questions/23436437/why-can-i-change-value-of-a-constant-in-javascript
 
-    counters[index] = { ...counter }; // only clone the object
-    counters[index].value++;
+  //   const counters = [...this.state.counters]; //cloning
+  //   const index = counters.indexOf(counter);
+  //   // counters[0].value++; //THIS WILL CHNAGE STATE property also....
 
-    console.log("after increment.. state value = ", this.state.counters[0].value);
-    console.log("after increment.. const value = ", counters[0].value);
-    this.setState({counters});
-  };
+  //   counters[index] = { ...counter }; // only clone the object
+  //   counters[index].value++;
 
-  handleDelete = counter => {
-    console.log(counter.id);
-    const counters = this.state.counters.filter(x => x.id !== counter.id);
-    this.setState({ counters });
-  };
+  //   console.log(
+  //     "after increment.. state value = ",
+  //     this.state.counters[0].value
+  //   );
+  //   console.log("after increment.. const value = ", counters[0].value);
+  //   this.setState({ counters });
+  // };
 
-  handleReset = () => {
-    console.log("reset");
-    const counters = this.state.counters.map(y => {
-      y.value = 0;
-      return y;
-    });
+  // handleDelete = counter => {
+  //   console.log(counter.id);
+  //   const counters = this.state.counters.filter(x => x.id !== counter.id);
+  //   this.setState({ counters });
+  // };
 
-    this.setState({ counters });
-  };
+  // handleReset = () => {
+  //   console.log("reset");
+  //   const counters = this.state.counters.map(y => {
+  //     y.value = 0;
+  //     return y;
+  //   });
+
+  //   this.setState({ counters });
+  // };
 
   render() {
     return (
       <div>
-        <button className="btn btn-primary" onClick={this.handleReset}>
+        <button className="btn btn-primary" onClick={() => this.props.onReset()}>
           Reset
         </button>
-        {this.state.counters.map(x => (
+        {this.props.counters.map(x => (
           <Counter
             key={x.id}
             counter={x}
             selected="true"
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
+            
           />
         ))}
       </div>
